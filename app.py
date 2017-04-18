9764a0f5db6779f4e3d61eadbf1d8db67e09bf0e
Luiz Filipe <moresco.luiz@gmail.co>
2017-04-18 14:59:43 -0300

    Improve the project structure

--- app.py
+++ app.py
@@ -1,14 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask
 from flask_bootstrap import Bootstrap
-
 
 app = Flask(__name__)
 Bootstrap(app)
 
-
-@app.route('/')
-def index():
-    return render_template('index.html')
+from views import *
 
 if __name__ == '__main__':
     app.run(debug=True)
