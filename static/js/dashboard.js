412d3753e5265ae1532cf6498e7d9eed6ac081c3
Luiz Filipe <moresco.luiz@gmail.co>
2017-04-18 18:58:54 -0300

    Populate modal with the informations about user

--- static/js/dashboard.js
+++ static/js/dashboard.js
@@ -4,8 +4,21 @@ DashboardModule = {
     },
 
     populateModal : function() {
-
-    }
-};
+        var row = $(this).parents('.new-user');
+        var name = $(row).find('.name').text();
+        var amount = $(row).find('.amount').text();
+        var phone = $(row).find('.phone').val();
+        var order = $(row).find('.order').text();
+        order = order.split(', ');
+        var modal = $('#moreInfoModal');
+        $(modal).find('#nameModal').text(name);
+        $(modal).find('#amountModal').text(amount);
+        $(modal).find('#phoneModal').text(phone);
+        $(modal).find('#listOrderModal').empty();
+        order.forEach(function(item, index) {
+            $(modal).find('#listOrderModal').append('<li>'+item+'</li>');
+        });
+    },
+}
 
 $(document).ready(DashboardModule.onLoad);
