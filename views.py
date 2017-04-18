ebcbe84c3b70ca84fef073a41cfebb3a0fa7d149
Luiz Filipe <moresco.luiz@gmail.co>
2017-04-18 16:41:36 -0300

    Show the users on template

--- views.py
+++ views.py
@@ -1,6 +1,14 @@
 from app import app
+from csv_reader import UserCSVReader, TrasactionCSVReader
 from flask import render_template
+
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    user_reader = UserCSVReader('usuarios.csv')
+    trasaction_reader = TrasactionCSVReader('transacoes.csv')
+    print(trasaction_reader.get_data())
+    return render_template('index.html',
+                           users=user_reader.get_data(),
+                           transactions=trasaction_reader.get_data(),
+            )
