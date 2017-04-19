2f0686a72a2b2a74fe4fc86d248154e9d480e1b1
Luiz Filipe <moresco.luiz@gmail.co>
2017-04-19 14:52:45 -0300

    Remove useful scripts

--- read_transacoes.py
+++
@@ -1,7 +0,0 @@
-# coding: utf-8
-with open('transacoes.csv','r') as f:
-    reader = csv.reader(f)
-    headers = next(reader)[1:]
-    for row in reader:
-        print(row)
-get_ipython().magic('save read_transacoes 13')
--- save_usuarios.py
+++
@@ -1,4 +0,0 @@
-# coding: utf-8
-def save_user(row):
-    users[row[0]] = {'name': row[1].strip(), 'phone': row[2].strip()}
-    
